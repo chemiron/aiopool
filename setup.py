079f5bbe0f21b8b60eb2f655519a8a0465db1c00
chemiron <chemiron34@gmail.com>
2015-07-21 10:13:10 +0300

    Setup info was added

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiopool",
-    version="0.0.1",
-    description="A library for running separated subprocesses with asyncio.",
+    version="0.0.1a1",
+    description="Subprocesses for asyncio",
+    long_description="A library for running separated subprocesses with asyncio.",
+    url="https://github.com/chemiron/aiopool.git",
+    author="Aliaksandr Nasukovich",
+    author_email="chemiron34@gmail.com",
+    license='MIT',
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+	"License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.4",
     ],
     packages=find_packages(),
